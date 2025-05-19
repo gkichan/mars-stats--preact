92a1840ce3e4647349e65ba81416dbc8ca3a681b
Yevhenii Porodko <gporodko@gmail.com>
2025-05-19 17:19:49 +0300

    koyeb api

--- scripts/config.js
+++ scripts/config.js
@@ -1,8 +1,9 @@
 "use strict";
 
 // TODO this config should be on env, separate from the code
+// NOTE: URLs should be without trailing slashes
 export const config = {
-  // apiUrl: "http://localhost:3000",
-  apiUrl: "https://mars-stats-be-express.onrender.com",
+  //  apiUrl: "http://localhost:3000",
+  apiUrl: "https://enthusiastic-farrah-porodkoindustries-d0d46820.koyeb.app",
   playersInGame: 3,
 };
