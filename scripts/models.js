60ca02fda9d362e72ad3ea06bbcb610c48004eaf
gkichan <gporodko@gmail.com>
2026-03-22 10:57:56 +0200

    add vitor and csm

--- scripts/models.js
+++ scripts/models.js
@@ -30,6 +30,8 @@ export const Corporation = {
   Beginner: 'Beginner',
   Astrodrill: 'Astrodrill',
   PharmacyUnion: 'Pharmacy Union',
+  Vitor: 'Vitor',
+  CheungShingMars: 'Cheung Shing Mars'
 };
 
 export const playersColors = {
